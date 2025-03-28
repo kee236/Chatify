673e04304b9dc790d31f5787fd87d32dbcf92d39
kee236 <31869356+kee236@users.noreply.github.com>
2025-03-28 22:03:58 +0700

    Update client.php

--- application/views/site/template/client.php
+++ application/views/site/template/client.php
@@ -1,1 +1,16 @@
 // view/site/template/client.php
+
+<div class="list-partner bg-line-dark pt-40 pb-40">
+    <div class="container text-center">
+        <div class="heading7 text-center">
+            Trusted by 50,000+ freelancers, marketing teams and agencies
+        </div>
+    </div>
+    <div class="container gap-32 row-gap-32 flex-between flex-wrap mt-32">
+        <div class="partner-item"><img src="<?= base_url('assets/images/partners/mega.svg'); ?>" alt="Mega"></div>
+        <div class="partner-item"><img src="<?= base_url('assets/images/partners/mian.svg'); ?>" alt="Mian"></div>
+        <div class="partner-item"><img src="<?= base_url('assets/images/partners/beta.svg'); ?>" alt="Beta"></div>
+        <div class="partner-item"><img src="<?= base_url('assets/images/partners/mian2.svg'); ?>" alt="Mian2"></div>
+        <div class="partner-item"><img src="<?= base_url('assets/images/partners/genvi.svg'); ?>" alt="Genvi"></div>
+    </div>
+</div>
