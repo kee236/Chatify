766245358d625c3915b0e9fc7ccc635907b60ebb
kee236 <31869356+kee236@users.noreply.github.com>
2025-05-02 09:05:48 +0700

    Update tailwind.config.js

--- application/views/site/ismartai-web/tailwind.config.js
+++ application/views/site/ismartai-web/tailwind.config.js
@@ -1,2 +1,4 @@
-// tailwind.config.js module.exports = { content: [ "./index.php", "./templates//*.php", "./assets/js//*.js" ], theme: { extend: { fontFamily: { prompt: ["Prompt", "sans-serif"] }, colors: { primary: '#4F46E5', secondary: '#A5B4FC' } }, }, plugins: [], }
+// tailwind.config.js
+
+ module.exports = { content: [ "./index.php", "./templates//*.php", "./assets/js//*.js" ], theme: { extend: { fontFamily: { prompt: ["Prompt", "sans-serif"] }, colors: { primary: '#4F46E5', secondary: '#A5B4FC' } }, }, plugins: [], }
 
