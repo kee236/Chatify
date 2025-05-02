1c5e983ee031f9b4d96250dc8d449cbc763d556f
kee236 <31869356+kee236@users.noreply.github.com>
2025-05-02 09:05:05 +0700

    Create tailwind.config.js

---
+++ application/views/site/ismartai-web/tailwind.config.js
@@ -0,0 +1,2 @@
+// tailwind.config.js module.exports = { content: [ "./index.php", "./templates//*.php", "./assets/js//*.js" ], theme: { extend: { fontFamily: { prompt: ["Prompt", "sans-serif"] }, colors: { primary: '#4F46E5', secondary: '#A5B4FC' } }, }, plugins: [], }
+
